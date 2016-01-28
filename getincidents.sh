9b91ab7fdb0d2e8aadba2e9c473f03b26c41e236
Marc Chauffour <karmoc@gmail.com>
2016-01-28 08:17:04 +0100

    requête Snow sur incidents

--- getincidents.sh
+++ getincidents.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl --user chauffourm:\$now1 https://brinkslatam.service-now.com/change_request_list.do?sysparm_query=active%3Dtrue%5EstateIN14%2C15%2C13%2C10%5Eu_it_resources_involvedSTARTSWITHBKFR\&CSV > public/changes.csv
+curl --user chauffourm:\$now1 https://brinkslatam.service-now.com/incident_list.do?sysparm_query=stateIN-5%2C2%2C1%2C8%5Eassignment_groupSTARTSWITHBKFR%5EimpactIN1%2C2%5Esys_created_on%3E%3Djavascript%3Ags.daysAgoStart(1)%5EcategoryINfr_access%2Cfr_office_software%2Cfr_business_application%2Cfr_hardware%2Cfr_messaging%2Cfr_mobility%2Cfr_network%2Cfr_security%2Cfr_system\&CSV > public/incidents.csv
