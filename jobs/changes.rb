eddcf6a74a5b1c0823d1bc6cb8f8f132de103b75
Marc Chauffour <karmoc@gmail.com>
2016-01-28 08:22:47 +0000

    mutualisation du code timetools

--- jobs/changes.rb
+++ jobs/changes.rb
@@ -1,15 +1,5 @@
 require 'csv'
 
-def humanize secs
-  secs, n = secs.divmod(3600)
-  [[24, :heures], [1000, :jours]].map{ |count, name|
-    if secs > 0
-      secs, n = secs.divmod(count)
-      "#{n.to_i} #{name}"
-    end
-  }.compact.reverse.join(' ')
-end
-
 SCHEDULER.every '1m', :first_in => 0 do |job|
   begin
     tweets = CSV.foreach("public/changes.csv", {:headers => true, :header_converters => :symbol}).map do |row|
