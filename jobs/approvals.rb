f90ede7500079f314103bc069865891b1699a323
Marc Chauffour <karmoc@gmail.com>
2016-02-03 20:58:41 +0000

    ajout des appobations

---
+++ jobs/approvals.rb
@@ -0,0 +1,54 @@
+require 'csv'
+require 'open-uri'
+require 'nokogiri'
+
+MAX_APPROV_OVERDUE = 15
+MAX_APPROV_AWAY = 15
+
+# config_file = File.dirname(File.expand_path(__FILE__)) + '/../timeline_data.yml'
+SCHEDULER.every '15m', :first_in => 0 do |job|
+  begin
+    # puts "execute getchanges.sh avec bash"
+    # puts system('bash ./getchanges.sh') ? "Run OK" : "KO!"
+    # config_file = File.dirname(File.expand_path(__FILE__)) + '/../config/environment.yml'
+    # config = YAML::load(File.open(config_file))
+    # command = config["cmd_c"]
+    # system(command)
+    url = "https://brinkslatam.service-now.com/sysapproval_approver_list.do?sysparm_query=state%3Drequested%5Edue_dateBETWEENjavascript%3Ags.daysAgoStart(#{MAX_APPROV_OVERDUE})%40javascript%3Ags.daysAgoEnd(#{-1*MAX_APPROV_AWAY})%5Esource_table%3Dchange_request%5EORsource_table%3Dsc_req_item%5Esysapproval.location.country%3DFrance&CSV"
+    
+    username = ENV["uname"]
+    password=ENV["pwd"]
+puts url
+    (open(url, :http_basic_authentication => [username, password]))
+    page = Nokogiri::HTML(open( url, :http_basic_authentication => [username, password] ))
+
+    tweets = CSV.parse(page.children.text, {:headers => true, :header_converters => :symbol}).map do |row|
+      ts = Time.parse(row[:due_date])
+      tstxt = "il y a"
+      ts = Time.now - ts
+      { approver: row[:approver], body: row[:short_description], when: Time.parse(row[:due_date]) }
+    end
+    puts tweets.inspect
+
+    grouped = tweets.group_by {|t| t[:approver]}
+    keys = grouped.keys # => ["food", "drink"]
+
+    arr = keys.map {|k| [k, grouped[k].reduce(0) {|t,h| t + 1}]}
+    # => [["food", 110], ["drink", 12]]
+
+    tasks = []
+    arr.sort { |a, b| a[1] <=> b[1] }.reverse.each do |t|
+      # puts t.inspect
+      tasks << {
+        name: t[0],
+        date: Date.today,
+        priority: 1,
+        formatted_date: t[1].to_s
+        }
+    end
+
+    max_items = 10
+    send_event "snow-approvals", { items: tasks.take(max_items) }
+
+  end
+end
--- jobs/changes.rb
+++ jobs/changes.rb
@@ -1,4 +1,9 @@
 require 'csv'
+require 'open-uri'
+require 'nokogiri'
+
+MAX_CHG_OVERDUE = 15
+MAX_CHG_AWAY = 15
 
 def state_to_color(state)
   case state
@@ -16,7 +21,7 @@ def state_to_color(state)
   end
 end
 
-config_file = File.dirname(File.expand_path(__FILE__)) + '/../timeline_data.yml'
+# config_file = File.dirname(File.expand_path(__FILE__)) + '/../timeline_data.yml'
 SCHEDULER.every '15m', :first_in => 0 do |job|
   begin
     # puts "execute getchanges.sh avec bash"
@@ -25,7 +30,8 @@ SCHEDULER.every '15m', :first_in => 0 do |job|
     # config = YAML::load(File.open(config_file))
     # command = config["cmd_c"]
     # system(command)
-    url = "https://brinkslatam.service-now.com/change_request_list.do?sysparm_query=active%3Dtrue%5EstateIN14%2C15%2C13%2C10%5Eu_it_resources_involvedSTARTSWITHBKFR&CSV"
+    url = "https://brinkslatam.service-now.com/change_request_list.do?sysparm_query=active%3Dtrue%5EstateIN14%2C15%2C13%2C10%5Elocation.country%3DFrance%5Erequested_by_dateBETWEENjavascript%3Ags.daysAgoStart(#{MAX_CHG_OVERDUE})%40javascript%3Ags.daysAgoEnd(#{-1*MAX_CHG_AWAY})&CSV"
+    
     username = ENV["uname"]
     password=ENV["pwd"]
 
@@ -55,7 +61,48 @@ SCHEDULER.every '15m', :first_in => 0 do |job|
     end
     timechanges = {"events" => changes}
     # puts e.to_yaml
-    File.open("timeline_data.yml", "w") { |file| file.write timechanges.to_yaml }
+    
+    # File.open("timeline_data.yml", "w") { |file| file.write timechanges.to_yaml }
     send_event('change_mentions', changes: tweets)
+ 
+    unless timechanges["events"].nil?
+      events =  Array.new
+      today = Date.today
+      no_event_today = true
+      timechanges["events"].each do |event|
+        # puts event.inspect
+        days_away = (Date.parse(event["date"]) - today).to_i
+        if (days_away >= 0) && (days_away <= MAX_CHG_AWAY) 
+          events << {
+            name: event["name"],
+            date: event["date"],
+            background: event["background"]
+          }
+          # puts event["date"] + " positif donne " + days_away.to_s + "days_away"
+        elsif (days_away < 0) && (days_away >= MAX_CHG_OVERDUE)
+          events << {
+            name: event["name"],
+            date: event["date"],
+            background: event["background"],
+            opacity: 0.5
+          }
+          # puts event["date"] + " negatif donne " + days_away.to_s + "days_away"
+        end
+
+        no_event_today = false if days_away == 0
+      end
+
+      if no_event_today
+        events << {
+          name: "TODAY",
+          date: today.strftime('%a %d %b %Y'),
+          background: "gold"
+        }
+      end
+
+      send_event("ags_timeline", {events: events})
+    else
+      puts "No events found :("
+    end
   end
 end
