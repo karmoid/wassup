04f11e5faa30bd2067a5cc60c8945d03ea3a0efa
Marc Chauffour <karmoc@gmail.com>
2016-02-04 17:30:49 +0100

    ajout VIP style

--- jobs/incidents.rb
+++ jobs/incidents.rb
@@ -31,20 +31,26 @@ SCHEDULER.every '1m', :first_in => 0 do |job|
     end
     # puts tweets.inspect
   send_event('incident_mentions', incidents: tweets)
-    
+
   url = "https://brinkslatam.service-now.com/incident_list.do?sysparm_query=active%3Dtrue%5Eu_task_for.vip%3Dtrue%5EstateNOT%20IN3%2C4%2C6%5EpriorityIN1%2C2&sysparm_view=&CSV"
   (open(url, :http_basic_authentication => [username, password]))
   page = Nokogiri::HTML(open( url, :http_basic_authentication => [username, password] ))
 
-    
-    tweets = CSV.parse(page.children.text, {:headers => true, :header_converters => :symbol}).map do |row|
+  hrows = [
+    { cols: [ {value: '#incident'}, {value: 'vip'}, {value: 'categorie'}, {value: 'sous-categ'}, {value: 'composant'}, {value: 'quand'} ] }
+  ]
+
+
+    rows = CSV.parse(page.children.text, {:headers => true, :header_converters => :symbol}).map do |row|
       ts = Time.parse(row[:sys_created_on])
       tstxt = "il y a"
       ts = Time.now - ts
-      { number: row[:number], name: row[:category] + ", "+ row[:subcategory], impact: row[:priority][3..row[:priority].length-1], body: row[:short_description], ci: row[:cmdb_ci], qui: row[:u_task_for], when: tstxt + " " + humanize(ts),
-        label:"#{row[:number]} - [#{row[:category]}/#{row[:subcategory]}] Impact #{row[:priority]}, #{row[:short_description]} " , value:"#{row[:u_task_for]} #{row[tstxt]} #{humanize(ts)}" }
+      { cols: [ {value: row[:number]}, {value: row[:u_task_for]}, {value: row[:category]}, {value: row[:subcategory]}, {value: row[:cmdb_ci]}, {value: tstxt + " " + humanize(ts)} ]}
+#      { number: row[:number], name: row[:category] + ", "+ row[:subcategory], impact: row[:priority][3..row[:priority].length-1], body: row[:short_description], ci: row[:cmdb_ci], qui: row[:u_task_for], when: tstxt + " " + humanize(ts),
+#        label:"#{row[:number]} - [#{row[:category]}/#{row[:subcategory]}] Impact #{row[:priority]}, #{row[:short_description]} " , value:"#{row[:u_task_for]} #{row[tstxt]} #{humanize(ts)}" }
     end
     # puts tweets.inspect
-    send_event('incident-vip', incidents: tweets)
+    # send_event('incident-vip', items: tweets)
+    send_event('incident-vip', { hrows: hrows, rows: rows } )
   end
 end
