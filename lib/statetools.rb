75fdd1416b5162473700718e6c3901d686f1ad32
Marc Chauffour <karmoc@gmail.com>
2016-10-12 15:16:10 +0200

    Correction sur Change (Couleur fonction de l'état)

--- lib/statetools.rb
+++ lib/statetools.rb
@@ -1,11 +1,17 @@
-def state_to_color(state)
+def state_to_color(state,close_success)
+  puts "#{state} & #{close_success.inspect}"
   case state
     when "Open" then ""
     when "Closed Successful" then "lightgreen"
     when "Closed Backed Out" then "red"
     when "Closed Rejected" then "red"
     when "Closed Cancelled" then "red"
-    when "Implementation" then "pink"
+    when "Implementation" then
+      if close_success==""
+        "pink"
+      else
+        close_success.downcase=="yes" ? "lightgreen" : "red"
+      end
     when "Classification" then "pink"
     when "Assessment & Planning" then "pink"
     when "SM Review" then "white"
