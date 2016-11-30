c20d8267f6838adaeb556073c242665463b4df27
Marc Chauffour <karmoc@gmail.com>
2016-11-30 09:26:59 +0100

    Déplacement du ticker dans la bonne zone

--- update_ticker.rb
+++ update_ticker.rb
@@ -2,24 +2,28 @@ require 'uri'
 require 'net/http'
 
 # url = URI("http://localhost:3030/widgets/ticker")
-url = URI("http://#{ENV['dashhost']}:3030/widgets/ticker")
+ticker_list = ["ticker"]
+ticker_list.each do |ticker|
+  puts "processing #{ticker} on http://#{ENV['dashhost']}:3030"
+  url = URI("http://#{ENV['dashhost']}:3030/widgets/#{ticker}")
 
-http = Net::HTTP.new(url.host, url.port)
+  http = Net::HTTP.new(url.host, url.port)
 
-# msg = ["Premier message","Deuxième message"]
+  # msg = ["Premier message","Deuxième message"]
 
-msg = []
-ARGV.each do |arg|
-   msg << arg
+  msg = []
+  ARGV.each do |arg|
+     msg << arg
+  end
+
+  request = Net::HTTP::Post.new(url)
+  request["authorization"] = 'Basic YWRtaW46Vk13YXJlMjAxNiE='
+  request["content-type"] = 'application/json'
+  request["cache-control"] = 'no-cache'
+  request["postman-token"] = 'f4d510be-766d-8170-e651-d8f936834d1b'
+  request.body = "{\"items\":#{msg.inspect},\"auth_token\": \"#{ENV['auth_token']}\"}"
+  puts request.body
+
+  response = http.request(request)
+  puts response.read_body
 end
-
-request = Net::HTTP::Post.new(url)
-request["authorization"] = 'Basic YWRtaW46Vk13YXJlMjAxNiE='
-request["content-type"] = 'application/json'
-request["cache-control"] = 'no-cache'
-request["postman-token"] = 'f4d510be-766d-8170-e651-d8f936834d1b'
-request.body = "{\"items\":#{msg.inspect},\"auth_token\": \"#{ENV['auth_token']}\"}"
-puts request.body
-
-response = http.request(request)
-puts response.read_body
