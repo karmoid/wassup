0ba82c18f984c7d122334a37df58560ae726848f
Marc Chauffour <karmoc@gmail.com>
2017-01-27 11:59:31 +0100

    correction cosmetique et Facts en Francais

--- jobs/chuck.rb
+++ jobs/chuck.rb
@@ -1,6 +1,7 @@
 require 'net/http'
 require 'json'
 require 'cgi'
+require 'htmlentities'
 
 #The Internet Chuck Norris Database
 server = "http://api.icndb.com"
@@ -17,27 +18,40 @@ proxy_pass = 'XXXXXXX'
 #The Array to take the names from
 teammembers = [['Mickey','Mouse']]
 
-SCHEDULER.every '2m', :first_in => 0 do |job|
+id_line = -1
+json_data = nil
+
+SCHEDULER.every '30s', :first_in => 0 do |job|
     random_member = teammembers.sample
     firstName = random_member[0]
     lastName = random_member[1]
 
-    #The uri to call, swapping in the team members name
-    uri = URI("#{server}/jokes/random?firstName=#{firstName}&lastName=#{lastName}&limitTo=[nerdy]")
+    if id_line == -1
+      #The uri to call, swapping in the team members name
+      # uri = URI("#{server}/jokes/random?firstName=#{firstName}&lastName=#{lastName}&limitTo=[nerdy]")
+      uri = URI("https://chucknorrisfacts.fr/api/get?type=%22txt%22;tri=%22top%22;nb=100")
+      #This is for when there is no proxy
+      res = Net::HTTP.get(uri)
 
-    #This is for when there is no proxy
-    res = Net::HTTP.get(uri)
+      #This is for when there is a proxy
+      #res = Net::HTTP::Proxy(proxy_host, proxy_port, proxy_user, proxy_pass).get(uri)
 
-    #This is for when there is a proxy
-    #res = Net::HTTP::Proxy(proxy_host, proxy_port, proxy_user, proxy_pass).get(uri)
+      #marshal the json into an object
+      json_data = JSON[res]
+      id_line=0
+      puts "#{json_data.size} Facts lus!"
+    end
 
-    #marshal the json into an object
-    j = JSON[res]
-
+    if json_data.size>0
+      joke = HTMLEntities.new.decode(CGI.unescapeHTML(json_data[id_line]["fact"]))
+      id_line += 1
+      id_line = -1 if id_line==json_data.size
+    else
+      joke = "Pas de Fact lu!"
+    end
     #Get the joke
-    joke = CGI.unescapeHTML(j['value']['joke'])
 
     #Send the joke to the text widget
-    send_event(id, { title: "#{firstName} #{lastName} Facts", text: joke })
+    send_event(id, { title: "Chuck Norris Fact ##{id_line}", text: joke })
 
 end
